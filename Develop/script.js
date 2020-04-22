9b8ad2e810804c4b898b22b2390c3f791493438b
Julia Clements <tiiedye@gmail.com>
2020-04-22 11:05:14 -0600

    added lower case

--- Develop/script.js
+++ Develop/script.js
@@ -6,7 +6,12 @@ var generatePassword = [];
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var lower = confirm("use lower case letters?")
+  if (lower) {
+    generatePassword.push("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+  }
+
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
