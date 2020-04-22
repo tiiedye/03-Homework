fe98eed72df228e87a2d24c52ee881751b0f3ef1
Julia Clements <tiiedye@gmail.com>
2020-04-22 12:31:35 -0600

    added notes to javascript to explain each element

--- script.js
+++ script.js
@@ -6,21 +6,25 @@ var generateBtn = document.querySelector("#generate");
 
 //function to generate password
     function writePassword() {
+      //adds lower case letters to generatePassword array
       var lower = confirm("use lower case letters?")
       if (lower) {
         generatePassword.push("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
       }
   
+      //adds upper case letters to generatePassword array
       var upper = confirm("use UPPER case letters?")
       if (upper) {
         generatePassword.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
       }
   
+      //adds special symbols to generatePassword array
       var special = confirm("use special symbols?")
       if (special) {
         generatePassword.push("!", "@", "#", "$", "%", "^", "&", "*")
       }
   
+      //adds numbers to generatePassword array
       var numeric = confirm("use numbers?")
       if (numeric) {
         generatePassword.push("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
@@ -28,6 +32,7 @@ var generateBtn = document.querySelector("#generate");
       
       var password = "";
 
+      //loop to randomly generate a password from generatePassword
       for (var i = 0; i < 10; i++) {
       password += generatePassword[Math.floor(Math.random() * generatePassword.length)];
       }
